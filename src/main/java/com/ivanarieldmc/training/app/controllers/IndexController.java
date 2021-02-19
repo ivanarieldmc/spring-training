034964d5fae7a059f07f8b8e1e8122525bd7fc42
ivanarieldmc <ivanarieldmc@gmail.com>
2021-02-19 17:06:40 -0300

    spring boot course lesson 212 models

--- src/main/java/com/ivanarieldmc/training/app/controllers/IndexController.java
+++ src/main/java/com/ivanarieldmc/training/app/controllers/IndexController.java
@@ -3,14 +3,32 @@ package com.ivanarieldmc.training.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ivanarieldmc.training.app.models.Usuario;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 	
 	@GetMapping({"/index","/","/home"})
 	public String index(Model model) {
-		model.addAttribute("titulo","hello spring framework");
+		model.addAttribute("titulo","Hello from the controller - Title");
+		model.addAttribute("h1","Hello from the controller - H1");
 		return "index";
+	}
+	
+	@GetMapping("/perfil")
+	public String perfil(Model model) {
+		
+		
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Ariel");
+		usuario.setApellido("Sparda");
+		model.addAttribute("usuario",usuario);
+		model.addAttribute("h1","Hello from the controller - H1");
+		model.addAttribute("titulo","Perfil del usuario "+usuario.getNombre()+" "+usuario.getApellido());
+		return "perfil";
 	}
 
 }
---
+++ src/main/java/com/ivanarieldmc/training/app/models/Usuario.java
@@ -0,0 +1,22 @@
+package com.ivanarieldmc.training.app.models;
+
+public class Usuario {
+	
+	String nombre;
+	String apellido;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+
+}
