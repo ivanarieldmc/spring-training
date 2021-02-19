78a4218c2354041a7dd13ee27be7760a9098f1cf
ivanarieldmc <ivanarieldmc@gmail.com>
2021-02-19 07:39:22 -0300

    commint inicial

---
+++ src/main/java/com/ivanarieldmc/training/app/StartingSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.ivanarieldmc.training.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingSpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartingSpringTestApplication.class, args);
+	}
+
+}
