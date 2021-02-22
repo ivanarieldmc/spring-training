73629300b2cc3385c1523d04dbd6e775a93944dd
ivanarieldmc <ivanarieldmc@gmail.com>
2021-02-22 15:18:16 -0300

    finished to lesson 225

--- src/main/java/com/ivanarieldmc/training/app/controllers/EjemploParamsController.java
+++ src/main/java/com/ivanarieldmc/training/app/controllers/EjemploParamsController.java
@@ -9,19 +9,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
-	
+
 	@GetMapping("/")
 	public String index() {
-		
+
 		return "params/index";
 	}
-	
+
 	@GetMapping("/string")
-	public String param(@RequestParam(required=false, defaultValue="mensaje standard, falta parametro") String texto, Model model) {
-		
-		model.addAttribute("resultado","El parametro enviado es: "+texto);
+	public String param(
+			@RequestParam(required = false, defaultValue = "mensaje standard, falta parametro") String texto,
+			Model model) {
+
+		model.addAttribute("resultado", "El parametro enviado es: " + texto);
 		return "params/ver";
 	}
-	
+
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo,@RequestParam Integer numero, Model model) {
+
+		model.addAttribute("resultado", "El saludo es: '"+saludo+"' y el numero es: '" + numero+"'");
+		return "params/ver";
+	}
 
 }
